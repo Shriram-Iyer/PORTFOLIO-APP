20b38bb7ce363cb4c4e7997172a3baa5e3d77eee
ShriramVI <shriram.v@unifo.in>
2023-11-06 11:38:05 +0530

    Portfolio updated

--- Portfolio.py
+++ Portfolio.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(layout='wide')
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1,2.5])
 with col1:
     st.image("images/photo.png", width=350)
 
@@ -18,7 +18,7 @@ st.write("Below you can find some apps I have build in Python. Feel free to Cont
 
 df = pd.read_csv("data.csv", sep=";")
 
-col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+col3, empty_col, col4 = st.columns([1.5, 0.25, 1.75])
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
